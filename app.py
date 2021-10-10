9b2578e85b1d5271d93c18f327ac0ca227cde62e
anxumaan5x <anxumaan@gmail.com>
2021-10-10 13:31:21 +0530

    favicon

--- app.py
+++ app.py
@@ -54,10 +54,10 @@ def num_decode(s):
 
 
 
-# @app.route('/favicon.ico')
-# def favicon():
-#     return send_from_directory(os.path.join(app.root_path, 'static'),
-#                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
                                
 
