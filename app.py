bebe092a3b374dd465069df5931ada5c78b59242
anxumaan5x <anxumaan@gmail.com>
2021-10-10 20:05:54 +0530

    postgre change 1

--- app.py
+++ app.py
@@ -95,7 +95,7 @@ def get_chats(from_id, to_id):
     # my_dict={{}}
     chats_dict={}
     # get_chat=Chat.query.filter(Chat.from_id.in_(my_list),Chat.to_id.in_(reversed_list)).order_by(Chat.time.asc()).all()
-    get_chat=Chat.query.filter(Chat.from_id.in_(my_list),Chat.to_id.in_(reversed_list)).order_by(Chat.time.asc()).distinct()
+    get_chat=Chat.query.filter(Chat.from_id.in_(my_list),Chat.to_id.in_(reversed_list)).order_by(Chat.time.asc())
     index=0
     for chat in get_chat:
         chats_dict[index]={}
@@ -117,7 +117,7 @@ def get_chats(from_id, to_id):
 
 #get all chats for one user
 def all_chats(to_id):
-    get_chat=Chat.query.filter_by(to_id=to_id).group_by(Chat.from_id).distinct()
+    get_chat=Chat.query.filter_by(to_id=to_id).group_by(Chat.from_id)
     my_dict={}
     user_received_chats_from=[]
     # print(get_chat)
