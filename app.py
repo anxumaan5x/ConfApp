ea95372bbe5af33208f2e23771516e55cffd0c58
anxumaan5x <anxumaan@gmail.com>
2021-10-10 13:35:39 +0530

    redirection

--- app.py
+++ app.py
@@ -140,7 +140,7 @@ client_secrets_file = "client_secret.json"
 flow = Flow.from_client_secrets_file(
     client_secrets_file=client_secrets_file,
     scopes=["https://www.googleapis.com/auth/userinfo.profile", "https://www.googleapis.com/auth/userinfo.email", "openid"],
-    redirect_uri="http://127.0.0.1:5000/callback"
+    redirect_uri="https://anotext.herokuapp.com/callback"
 )
 
 
